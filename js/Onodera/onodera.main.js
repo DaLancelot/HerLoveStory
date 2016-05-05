e0ace7889e2043576e6d0377355b9b7befe99b47
Paulo Nunes <amiguencio@hotmail.com>
2016-05-05 20:55:10 +0100

    Fixed bugs with main code
    Fixed bugs on the main code for the game. Now it works properly. Next
    update will save data on the browser.

--- js/Onodera/onodera.main.js
+++ js/Onodera/onodera.main.js
@@ -1,7 +1,26 @@
 var mainStory = {
-  1: "Hey Yuuta, would you like to go to the mall today?|Onodera",
-  2: "Sure why not? it will be fun|Yuuta",
-  3: "Where would you like to go?|Yuuta",
-  4: "I want to visit some shops where a friend told me they had cute teddies|Onodera",
-  5: "We will have  alot of fun for sure|Yuuta",
+  "1": "Hey Yuuta, would you like to go to the mall today?|Onodera",
+  "2": "Sure why not? it will be fun|Yuuta",
+  "3": "Where would you like to go?|Yuuta",
+  "4": "I want to visit some shops where a friend told me they had cute teddies|Onodera",
+  "5": "We will have  alot of fun for sure|Yuuta",
+  "6": "quest/What time do you want to go?|Onodera",
+  "7": "Cool!",
+};
+
+var quests = {
+  "6": {
+    "opOn" : {
+      "text": "What about 1PM?",
+      "jumpTo": "1",
+    },
+    "opTw" : {
+      "text": "What about 4PM?",
+      "jumpTo": "2",
+    },
+    "opTh" : {
+      "text": "What about 11AM?",
+      "jumpTo": "7",
+    },
+  },
 };
--- js/core.js
+++ js/core.js
@@ -3,9 +3,10 @@ var whereWasI = 1,
     character = divideMainStory[1],
     text = divideMainStory[0],
     currentChar = character;
-console.log(text + " -" + character);
-$("#dlg-container").prepend("<div id='dlg-sect'><h2>" + character + "</h2><p>" + text + "</p></div>");
+//$("#dlg-container").prepend("<div id='dlg-sect'><h2>" + character + "</h2><p>" + text + "</p></div>");
 
+$("div#next-slide").click(nextSlide);
+// old version
 function next () {
   whereWasI++;
   if (whereWasI == mainStory.length || mainStory[whereWasI] == undefined) {
@@ -16,20 +17,89 @@ function next () {
         text = divideMainStory[0];
     var dialogueBoxHTML = "<div id='dlg-sect'><h2>" + character + "</h2><p>" + text + "</p></div>",
         dialogueSameChar = $("#dlg-container #dlg-sect p").text(text);
-    if ($("#dlg-container").children().length == 3) {
-      $("#dlg-container #dlg-sect:last-child").remove();
+    if ($("#dlg-container").children().length >= 3) {
       //check if character is the same as the sentence before
       if (currentChar == character) {
         $("#dlg-container #dlg-sect:first-child p").text(text);
         console.log(currentChar);
       } else if (currentChar != character){
+        $("#dlg-container #dlg-sect:nth-child(2)").addClass("slide");
         $("#dlg-container").prepend(dialogueBoxHTML);
         console.log(currentChar);
       };
       return currentChar = character;
-    } else {
-      $("#dlg-container").prepend(dialogueBoxHTML);
     };
-    console.log(text + " -" + character)
+    console.log(text + " -" + character);
   };
+};
+
+// testing code
+function nextSlide () {
+  /*if (whereWasI == mainStory.length || mainStory[whereWasI] == undefined) {
+    console.info("Cya later <3");
+  } else */if (mainStory[whereWasI].substring(0, 5) == "quest") {
+    var splitIt = mainStory[whereWasI].split("/"),
+        q = splitIt[0],
+        t = splitIt[1],
+        tSplit = t.split("|"),
+        tText = tSplit[0],
+        tChar = tSplit[1];
+    var questSelector = quests[whereWasI];
+    var dialogueBoxHTML = "<div id='dlg-sect'><h2>" + tChar + "</h2><p>" + tText + "</p><ul id='dlg-quest' quest='6'><li id=" + questSelector.opOn.jumpTo + ">" + questSelector.opOn.text + "</li><li id=" + questSelector.opTw.jumpTo + ">" + questSelector.opTw.text + "</li><li id=" + questSelector.opTh.jumpTo + ">" + questSelector.opTh.text + "</li></ul></div>";
+    $("#dlg-container #dlg-sect:nth-child(2)").addClass("slide");
+    $("#dlg-container").prepend(dialogueBoxHTML);
+    $("#next-slide").fadeOut(500).addClass("easily-hide");
+  } else {
+    var divideMainStory = mainStory[whereWasI].split("|"),
+        char = divideMainStory[1],
+        text = divideMainStory[0];
+        oldTextfromChar = text,
+        oldChar = char;
+    var dialogueBoxHTML = "<div id='dlg-sect'><h2>" + char + "</h2><p>" + text + "</p></div>",
+        dialogueSameChar = $("#dlg-container #dlg-sect p").text(text);
+    if ($("#dlg-container").children().length >= 3) {
+      if (currentChar == char) {
+        $("#dlg-container #dlg-sect:first-child h2").text(char);
+        $("#dlg-container #dlg-sect:first-child p").text(text);
+        $("#dlg-container #dlg-sect:nth-child(2)").text("");
+        $("#dlg-container #dlg-sect:first-child ul").remove();
+        console.log(currentChar + " opt 1");
+      } else if (currentChar != char){
+        $("#dlg-container #dlg-sect:nth-child(2)").addClass("slide");
+        $("#dlg-container").prepend(dialogueBoxHTML);
+        console.log(currentChar + " opt 2");
+      };
+    };
+    $("#next-slide").fadeIn(500).removeClass("easily-hide");
+    console.log(text + " -" + char);
+    console.log(whereWasI);
+  };
+  progress(whereWasI);
+  whereWasI++;
+  return currentChar = char;
+};
+
+$(document).on("click", "li", function () {
+  var elId = $(this).attr("id");
+  whereWasI = elId;
+  nextSlide();
+  console.log("did this." + elId);
+});
+
+$(document).keypress(function( event ) {
+  if (event.which == 32) {
+     if (!($("#next-slide").hasClass("easily-hide"))) {
+       nextSlide();
+     };
+  };
+});
+// TODO: progress in story: divide 100 by the story length
+
+function progress(prog) {
+  var progressBar = $("span#progress"),
+      storyLength = Object.keys(mainStory).length,
+      progressCSS = progressBar.css("width"),
+      progressCalculated = ((whereWasI - 1) * (100 / (storyLength - 1))) + "%";
+      console.log(progressCalculated);
+  progressBar.animate({"width" : progressCalculated});
 };
